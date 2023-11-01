2926fc39b4dc460a6f0892777c3e99c77270aeb6
Aldo Jesus Martínez Larios <1722110193@utectulancingo.edu.mx>
2023-11-01 16:49:17 +0000

    UPDATE main.py

--- main.py
+++ main.py
@@ -26,28 +26,57 @@ class Contacto(BaseModel):
     nombre : str
     telefono : str
 
+@app.post("/contactos")
+async def crear_contacto(contacto: Contacto):
+    """Crea un nuevo contacto."""
+    # TODO Inserta el contacto en la base de datos y responde con un mensaje
+    connection = conn.cursor()
+    conn.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+              (contacto.email, contacto.nombre, contacto.telefono))
+    conn.commit()
+    return contacto
+
 @app.get("/contactos")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
-    # DONE Consulta todos los contactos de la base de datos y los envia en un JSON
+    # Consulta todos los contactos de la base de datos y los envía en un JSON
     c = conn.cursor()
     c.execute('SELECT * FROM contactos')
     response = []
-    for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
-        response.append(contacto)
+    for row in c.fetchall():
+        contacto = Contacto(email=row[0], nombre=row[1], telefono=row[2])
+        response.append(contacto.dict())
     return response
 
+
 @app.get("/contactos/{email}")
-async def buscar_contacto(email: str = Query(..., title="Correo electrónico", description="Correo electrónico del contacto a buscar")):
-    """Busca un contacto por correo electrónico."""
-    # DONE Consulta el contacto con el correo electrónico proporcionado en la base de datos.
+async def obtener_contacto(email: str):
+    """Obtiene un contacto por su email."""
+    # Consulta el contacto por su email
     c = conn.cursor()
     c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
-    result = c.fetchone()
-    
-    if result is None:
-        raise HTTPException(status_code=404, detail="El contacto no se encontró")
-    
-    contacto = {"email": result[0], "nombre": result[1], "telefono": result[2]}
-    return contacto
+    row = c.fetchone()
+    if row:
+        contacto = Contacto(email=row[0], nombre=row[1], telefono=row[2])
+        return contacto.dict()
+    else:
+        return None
+
+@app.put("/contactos/{email}")
+async def actualizar_contacto(email: str, contacto: Contacto):
+    """Actualiza un contacto."""
+    # Actualiza el contacto en la base de datos
+    c = conn.cursor()
+    c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
+              (contacto.nombre, contacto.telefono, email))
+    conn.commit()
+    return contacto.dict()
+
+@app.delete("/contactos/{email}")
+async def eliminar_contacto(email: str):
+    """Elimina un contacto."""
+    # Elimina el contacto de la base de datos
+    c = conn.cursor()
+    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+    conn.commit()
+    return {"message": "Contacto eliminado"}
