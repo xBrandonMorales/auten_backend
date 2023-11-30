1a86ffc54bc3783c2cb10d64c2d4cdc2c9729f22
Aldo Jesus Martínez Larios <122809833+AldoJesusMartinezLarios@users.noreply.github.com>
2023-11-29 18:46:29 -0600

    CREATED all endpoint

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fastapi
 import sqlite3
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
 
 # Crea la base de datos
 conn = sqlite3.connect("sql/contactos.db")
@@ -25,62 +26,121 @@ class Contacto(BaseModel):
     nombre : str
     telefono : str
 
+from fastapi import HTTPException
+
 @app.post("/contactos")
 async def crear_contacto(contacto: Contacto):
     """Crea un nuevo contacto."""
-    # TODO Inserta el contacto en la base de datos y responde con un mensaje
-    connection = conn.cursor()
-    conn.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
-              (contacto.email, contacto.nombre, contacto.telefono))
-    conn.commit()
-    return contacto
+    try:
+        # Verifica si el email ya existe en la base de datos
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos WHERE email = ?', (contacto.email,))
+        existing_contact = c.fetchone()
+
+        if existing_contact:
+            raise HTTPException(status_code=400, detail={"mensaje": "El email ya existe"})
+
+        # Inserta el nuevo contacto en la base de datos
+        c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+                  (contacto.email, contacto.nombre, contacto.telefono))
+        conn.commit()
+
+        return {"mensaje": "Contacto insertado correctamente"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"mensaje": "Error al consultar los datos"})
+
 
 @app.get("/contactos")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
-    # Consulta todos los contactos de la base de datos y los envía en un JSON
-    c = conn.cursor()
-    c.execute('SELECT * FROM contactos')
-    response = []
-    for row in c.fetchall():
-        contacto = Contacto(email=row[0], nombre=row[1], telefono=row[2])
-        response.append(contacto.dict())
-    return response
+    try:
+        # Consulta todos los contactos de la base de datos y los envía en un JSON
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos')
+        response = []
+        for row in c.fetchall():
+            contacto = {
+                "email": row[0],
+                "nombre": row[1],
+                "telefono": row[2]
+            }
+            response.append(contacto)
+
+        if response:
+            return response
+        else:
+            raise HTTPException(status_code=202, detail="No hay registros")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"mensaje": "Error al consultar los datos"})
 
 
 @app.get("/contactos/{email}")
 async def obtener_contacto(email: str):
     """Obtiene un contacto por su email."""
-    # Consulta el contacto por su email
-    c = conn.cursor()
-    c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
-    row = c.fetchone()
-    if row:
-        contacto = Contacto(email=row[0], nombre=row[1], telefono=row[2])
-        return contacto.dict()
-    else:
-        return None
+    try:
+        # Consulta el contacto por su email
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+        row = c.fetchone()
+
+        if row:
+            contacto = {
+                "email": row[0],
+                "nombre": row[1],
+                "telefono": row[2]
+            }
+            return contacto
+        else:
+            raise HTTPException(status_code=404, detail={"mensaje": "El email no existe"})
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"mensaje": "Error al consultar los datos"})
 
 @app.put("/contactos/{email}")
 async def actualizar_contacto(email: str, contacto: Contacto):
     """Actualiza un contacto."""
-    # Asegúrate de que los datos cumplen con las validaciones del modelo Contacto
-    if contacto.nombre is None or contacto.telefono is None:
-        raise fastapi.HTTPException(status_code=422, detail="Nombre y teléfono son campos obligatorios")
+    try:
+        if contacto.nombre is None or contacto.telefono is None:
+            raise HTTPException(status_code=422, detail="Nombre y teléfono son campos obligatorios")
 
-    # Actualiza el contacto en la base de datos
-    c = conn.cursor()
-    c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
-              (contacto.nombre, contacto.telefono, email))
-    conn.commit()
-    return contacto.dict()
+        # Verifica si el contacto con el email proporcionado existe
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+        existing_contact = c.fetchone()
+
+        if not existing_contact:
+            raise HTTPException(status_code=404, detail={"mensaje": "El ID contacto no existe"})
+
+        # Actualiza el contacto en la base de datos
+        c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
+                  (contacto.nombre, contacto.telefono, email))
+        conn.commit()
+
+        return {"mensaje": "Contacto actualizado correctamente"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"mensaje": "Error al consultar o actualizar los datos"})
 
 
 @app.delete("/contactos/{email}")
 async def eliminar_contacto(email: str):
     """Elimina un contacto."""
-    # Elimina el contacto de la base de datos
-    c = conn.cursor()
-    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
-    conn.commit()
-    return {"message": "Contacto eliminado"}
+    try:
+        # Verifica si el contacto con el email proporcionado existe
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+        existing_contact = c.fetchone()
+
+        if not existing_contact:
+            raise HTTPException(status_code=404, detail={"mensaje": "El email del contacto no existe"})
+
+        # Elimina el contacto de la base de datos
+        c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+        conn.commit()
+
+        return {"mensaje": "Contacto borrado correctamente"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"mensaje": "Error al consultar o eliminar los datos"})
